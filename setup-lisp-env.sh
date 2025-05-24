d58a09058df1253da4c203889d2f74d5c34c8626
Vee Satayamas <vsatayamas@gmail.com>
2025-05-24 09:18:43 +0700

    Add setup Lisp script

---
+++ setup-lisp-env.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+rm -rf ~/quicklisp
+rm -f quicklisp.lisp
+wget https://beta.quicklisp.org/quicklisp.lisp
+sbcl --load quicklisp.lisp \
+        --eval '(quicklisp-quickstart:install)' \
+        --eval '(ql-util:without-prompting (ql:add-to-init-file))' \
+        --quit
+
+rm -f quicklisp.lisp
+
+sbcl --eval '(ql-dist:install-dist "http://dist.ultralisp.org/" :prompt nil)' --quit
+
+if [ -e ~/.config/common-lisp ]; then
+    cp -rp ~/.config/common-lisp ~/.config/common-lisp.bak-$(date -I)-$$
+fi
+
+mkdir -p ~/.config/common-lisp
+
+cat <<EOF > ~/.config/common-lisp/source-registry.conf
+(:source-registry
+     (:tree (:home "Develop/lisp"))
+     :inherit-configuration)
+EOF
+
